7b0e1c5df5636791880b08ded6708b650b09f4e9
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-10-20 10:05:23 +0630

    feat: setup config service

--- src/services/Config.ts
+++ src/services/Config.ts
@@ -3,17 +3,20 @@ import { Context, Effect, Layer } from "effect";
 export class Config extends Context.Tag("Config")<Config, {
   readonly getConfig: Effect.Effect<{
     connection: string;
+    port: number;
   }>;
 }>() {}
 
 export const ConfigLive = Layer.succeed(Config, {
   getConfig: Effect.succeed({
     connection: "",
+    port: 5000,
   }),
 });
 
 export const ConfigTest = Layer.succeed(Config, {
   getConfig: Effect.succeed({
     connection: "",
+    port: 5000,
   }),
 });
