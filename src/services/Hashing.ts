6777a9267408a143c5f896a0a9129223975b80c0
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-11-05 16:13:14 +0630

    implement hashing service

--- src/services/Hashing.ts
+++ src/services/Hashing.ts
@@ -1,11 +1,37 @@
-import { Effect as Ef, Context, Data } from "effect";
+import { Context, Data, Effect as Ef, Layer } from "effect";
+import { hash, verify } from "argon2";
 
 export class HashingError extends Data.TaggedError("HashingError")<{ message: string }> {}
+export class HashNotMatchError extends Data.TaggedError("HashNotMatchError")<{ message: string }> {}
 
 export class Hashing extends Context.Tag("HashingService")<
   Hashing,
   {
     readonly hash: (passwd: string) => Ef.Effect<string, HashingError>;
-    readonly verify: (hash: string, passwd: string) => Ef.Effect<boolean, HashingError>;
+    readonly verify: (
+      hash: string,
+      passwd: string,
+    ) => Ef.Effect<true, HashingError | HashNotMatchError>;
   }
 >() {}
+
+export const HashingLive = Layer.effect(
+  Hashing,
+  Ef.gen(function* () {
+    return {
+      verify: (hash, passwd) =>
+        Ef.tryPromise(() => verify(hash, passwd)).pipe(
+          Ef.if({
+            onTrue: () => Ef.succeed(true as const),
+            onFalse: () => new HashNotMatchError({ message: "password does not match" }),
+          }),
+          Ef.catchTag("UnknownException", (e) => new HashingError({ message: e.message })),
+        ),
+
+      hash: (passwd) =>
+        Ef.tryPromise(() => hash(passwd)).pipe(
+          Ef.catchTag("UnknownException", (e) => new HashingError({ message: e.message })),
+        ),
+    };
+  }),
+);
